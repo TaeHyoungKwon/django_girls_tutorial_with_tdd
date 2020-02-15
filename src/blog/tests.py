80f5eaac9649652c2a4430a2f441e52dc5adbb12
TaeHyoungKwon <kth5604@gmail.com>
2020-02-15 09:47:19 +0900

    Refactor extracting method duplcated code

--- src/blog/tests.py
+++ src/blog/tests.py
@@ -1,21 +1,24 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
+from django.utils import timezone
+
+from freezegun import freeze_time
 
 from blog.models import Post
 
 
 class TestPostModel(TestCase):
-    def test_post_model_instance_name_should_be_post_title(self):
-        # Given: Set test user and post
-        test_user = User.objects.create_user(
+    def setUp(self) -> None:
+        self.test_user = User.objects.create_user(
             email="kwon5604@naver.com", username="thkwon", password="password"
         )
-        test_post = Post.objects.create(
-            author=test_user, title="title", text="text"
+        self.test_post = Post.objects.create(
+            author=self.test_user, title="title", text="text"
         )
 
+    def test_post_model_instance_name_should_be_post_title(self):
         # When: Get post instance
-        post_instance = Post.objects.get(id=test_post.id)
+        post_instance = Post.objects.get(id=self.test_post.id)
 
         # Then: post_instance.__str__ should be test_post.title
-        self.assertEqual(post_instance.__str__(), test_post.title)
+        self.assertEqual(post_instance.__str__(), self.test_post.title)
