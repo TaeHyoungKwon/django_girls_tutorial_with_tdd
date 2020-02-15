767cc0a86c6857ca9908f25a3d3f2b709e4fdbe3
TaeHyoungKwon <kth5604@gmail.com>
2020-02-15 09:26:29 +0900

    Add test about post model instance name

--- src/blog/tests.py
+++ src/blog/tests.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from blog.models import Post
+
+
+class TestPostModel(TestCase):
+    def test_post_model_instance_name_should_be_post_title(self):
+        # Given: Set test user and post
+        test_user = User.objects.create_user(
+            email="kwon5604@naver.com", username="thkwon", password="password"
+        )
+        test_post = Post.objects.create(
+            author=test_user, title="title", text="text"
+        )
+
+        # When: Get post instance
+        post_instance = Post.objects.get(id=test_post.id)
+
+        # Then: post_instance.__str__ should be test_post.title
+        self.assertEqual(post_instance.__str__(), test_post.title)
